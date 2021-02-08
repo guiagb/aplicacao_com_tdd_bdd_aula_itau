984b12afb7ab350d65bf307e26f27ddee92e6741
GUILHERME ANDRE GARCIA BUENO <987301152@itaud.des.ihf>
2021-02-08 12:04:09 -0300

    minhas alteracaoes

--- test/cliente.test.js
+++ test/cliente.test.js
@@ -1,37 +1,37 @@
 const Cliente = require("../models/cliente")
 
 test('Quando eu digitar um CPF para um cliente válido, deveria retornar true', () => {
-  var cliente = new Cliente("Danilo", "64440783007")
+  var cliente = new Cliente("Guilherme", "42436598851")
   expect(cliente.validarCPF()).toBe(true);
 });
 
 test('Quando eu digitar um CPF para um cliente válido formatado, deveria retornar true', () => {
-  var cliente = new Cliente("Danilo", "644.407.830-07")
+  var cliente = new Cliente("Guilherme", "424.365.988-51")
   expect(cliente.validarCPF()).toBe(true);
 });
 
 test('Quando eu digitar um CPF não definido, deveria retornar false', () => {
-  var cliente = new Cliente("Danilo", undefined)
+  var cliente = new Cliente("Guilherme", undefined)
   expect(cliente.validarCPF()).toBe(false);
 });
 
 test('Quando eu digitar um CPF 00000000000 deveria retornar false', () => {
-  var cliente = new Cliente("Danilo", "00000000000")
+  var cliente = new Cliente("Guilherme", "00000000000")
   expect(cliente.validarCPF()).toBe(false);
 });
 
 test('Quando eu digitar um CPF 000000000000 deveria retornar false', () => {
-  var cliente = new Cliente("Danilo", "000000000000")
+  var cliente = new Cliente("Guilherme", "000000000000")
   expect(cliente.validarCPF()).toBe(false);
 });
 
 test('Quando eu digitar um CPF 0000000000 deveria retornar false', () => {
-  var cliente = new Cliente("Danilo", "0000000000")
+  var cliente = new Cliente("Guilherme", "0000000000")
   expect(cliente.validarCPF()).toBe(false);
 });
 
 test('Quando eu digitar um CPF 11111111111 deveria retornar false', () => {
-  var cliente = new Cliente("Danilo", "11111111111")
+  var cliente = new Cliente("Guilherme", "11111111111")
   expect(cliente.validarCPF()).toBe(false);
 });
 
